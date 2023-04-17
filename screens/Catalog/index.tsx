163406ef221f4637fdd875b4f410f4a173d56230
gustavocarvallo <gustavocarvallo@sirius.com.ar>
2023-04-17 10:32:35 -0300

    feat: integrate Catalog screen with redux

--- screens/Catalog/index.tsx
+++ screens/Catalog/index.tsx
@@ -1,41 +1,43 @@
-import { FlatList, Text, View } from "react-native";
-import { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import { useEffect } from "react";
 import { getAllGames } from "../../api";
-import { Game } from "../../types";
 import GameCard from "../../components/GameCard";
 import styles from "./styles";
 import Button from "../../components/Button";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setGames,
+  setGamesError,
+  setGamesLoading,
+} from "../../redux/gameSlice";
+import { RootState } from "../../redux/store";
 
 const Catalog = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [loadError, setLoadError] = useState(false);
+  const dispatch = useDispatch();
+  const games = useSelector((state: RootState) => state.game.games);
+  const { error, loading } = useSelector((state: RootState) => ({
+    error: state.game.gamesError,
+    loading: state.game.gamesLoading,
+  }));
 
   useEffect(() => {
     fetchGame();
   }, []);
 
   const fetchGame = () => {
+    dispatch(setGamesLoading());
     getAllGames()
       .then((response) => {
-        setGames(response);
+        // note: this timeout was added to delay the request so the loader is visible
+        setTimeout(() => dispatch(setGames(response)), 1500)
       })
-      .catch(() => {
-        setLoadError(true);
-      });
+      .catch(() => dispatch(setGamesError()));
   };
 
   return (
     <View>
-      {!loadError ? (
-        <FlatList
-          data={games}
-          renderItem={({ item, index }) => {
-            const customStyles = index === 0 ? { card: { marginTop: 48 } } : {};
-            return <GameCard game={item} styles={customStyles} />;
-          }}
-          ItemSeparatorComponent={() => <View style={styles.separator} />}
-        />
-      ) : (
+      {loading && !error && <ActivityIndicator size={"large"}/>}
+      {error && !loading && (
         <View style={styles.errorContainer}>
           <Text style={styles.errorText}>
             An error occurred while loading the games
@@ -46,6 +48,17 @@ const Catalog = () => {
             onPress={fetchGame}
           />
         </View>
+      )}
+      {!loading && !error && (
+        <FlatList
+          data={games}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item, index }) => {
+            const customStyles = index === 0 ? { card: { marginTop: 48 } } : {};
+            return <GameCard game={item} styles={customStyles} />;
+          }}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+        />
       )}
     </View>
   );
