773db6ba9f254bcec1fde5c14eb0b928e9f197a1
gustavocarvallo <gustavocarvallo@sirius.com.ar>
2023-04-16 22:44:28 -0300

    fix: handle image not found error on `GameCard`

--- components/GameCard/index.tsx
+++ components/GameCard/index.tsx
@@ -1,13 +1,21 @@
-import {ImageBackground, View, Text, TouchableOpacity, ToastAndroid} from "react-native";
+import {
+  ImageBackground,
+  View,
+  Text,
+  TouchableOpacity,
+  ToastAndroid,
+} from "react-native";
 import styles from "./styles";
 import { Game } from "../../types";
-import {useCallback, useMemo} from "react";
+import { useCallback, useMemo, useState } from "react";
 
 interface GameCardProps {
   game: Game;
 }
 
 const GameCard = ({ game }: GameCardProps) => {
+  const [imageNotFoundError, setImageNotFoundError] = useState(false);
+
   const gameImageSrc = useMemo(() => {
     if (game.steamUrl) {
       // game app id is the last part of the `steamUrl`
@@ -19,20 +27,24 @@ const GameCard = ({ game }: GameCardProps) => {
   }, [game.steamUrl]);
 
   const onPress = useCallback(() => {
-    ToastAndroid.show(game.title ?? "Game tittle not available", ToastAndroid.SHORT)
-  }, [game.title])
-
+    ToastAndroid.show(
+      game.title ?? "Game tittle not available",
+      ToastAndroid.SHORT
+    );
+  }, [game.title]);
 
   // todo: we might want to add a default image
   return (
     <TouchableOpacity style={styles.card} activeOpacity={0.6} onPress={onPress}>
-      {gameImageSrc ? (
-        <ImageBackground source={{ uri: gameImageSrc }} style={styles.image} />
+      {gameImageSrc && !imageNotFoundError ? (
+        <ImageBackground
+          source={{ uri: gameImageSrc }}
+          style={styles.image}
+          onError={() => setImageNotFoundError(true)}
+        />
       ) : (
         <View style={styles.imageNotAvailableContainer}>
-          <Text style={styles.imageNotAvailableText}>
-            Image not available
-          </Text>
+          <Text style={styles.imageNotAvailableText}>Image not available</Text>
         </View>
       )}
     </TouchableOpacity>
